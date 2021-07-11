059d41f1e513417fecf0b6254157c71f8b07b7a4
wesley_wisch <wesley_wisch@hotmail.com>
2021-07-11 14:32:11 -0400

    Desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from '../App';
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+type ContentProps ={
+  selectedGenre: string;
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard 
+            key={movie.imdbID} 
+            title={movie.Title} 
+            poster={movie.Poster} 
+            runtime={movie.Runtime} 
+            rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
